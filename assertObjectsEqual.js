1b3beb03988fd4b2cb15bf98ae18742a5311c377
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-25 22:09:25 +0000

    fixing error with util library's inspect function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -16,12 +16,14 @@ const eqObjects = function(object1, object2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
-  
+  const inspect = require('util').inspect; // <= add this line
+  console.log(`Example label: ${inspect(actual)}`);
   if (!eqObjects(actual, expected)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   }
 };
 
 assertObjectsEqual({a: "hi"}, {a: "hi"});
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' });
