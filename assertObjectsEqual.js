79a011939562e84e606a1d411693cfdcf10a35af
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-25 22:08:20 +0000

    [object Object] === [object Object] error in terminal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,27 @@
+const eqObjects = function(object1, object2) {
+
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for(let key in object1) {
+      if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if(!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]){
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  
+  if (!eqObjects(actual, expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+assertObjectsEqual({a: "hi"}, {a: "hi"});
