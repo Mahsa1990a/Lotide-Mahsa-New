85380aebb93c964190ee5d41642a57973ba53dd8
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-26 20:40:20 +0000

    last changes, use break

--- takeUntil.js
+++ takeUntil.js
@@ -21,20 +21,21 @@ const assertArraysEqual = function(actual, expected) {
 
 const takeUntil = function(array, callback) {
   let result = [];
-  for (let i = 0; i < array.length; i++) {
-    //console.log(array[i]);
-    if (callback(array[i])) {
-      return result;
+  for (let item of array) {
+    //console.log("item", item);
+    if(callback(item)) {
+      break; //we need to stop loop till it is true
     }
-    result.push(array[i]);
+    result.push(item);
   }
+  //result.push(item); item is not defined
   return result;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);  //[ 1, 2, 5, 7, 2 ]
-//console.log([1, 2, 5, 7, 2, -1, 2, 4, 5].takeUntil(x => x < 0));
+
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
