c366ac68a05752c011ba30bb4ae3692159ce5a8d
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-30 19:32:32 +0000

    Refactor

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,19 +1,4 @@
-const eqObjects = function(object1, object2) {
-
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for(let key in object1) {
-      if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if(!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]){
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
@@ -27,3 +12,5 @@ const assertObjectsEqual = function(actual, expected) {
 
 assertObjectsEqual({a: "hi"}, {a: "hi"});
 assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' });
+
+module.exports = assertObjectsEqual;
