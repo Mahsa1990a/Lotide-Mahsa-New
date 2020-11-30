439beb7477bbbd9f57be909ea65248e7f5e1bfe5
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-30 01:31:42 +0000

    refactor test codes

--- assertEqual.js
+++ assertEqual.js
@@ -2,15 +2,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     //console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     //console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Ok", 1);
-assertEqual(-5, 1);
+module.exports = assertEqual;
