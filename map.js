49cdbd0b682471e0b72d4c57c75c15cb710620e2
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-11-30 19:45:30 +0000

    Refactor

--- map.js
+++ map.js
@@ -1,23 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (!eqArrays(actual, expected)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
+//const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const words = ["ground", "control", "to", "major", "tom"];
 const map = function(array, callback) {
@@ -41,3 +23,7 @@ console.log(result1);
 assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
 assertArraysEqual(map(["Hi", "Hello", "bye"], x => x.length), [2]);
 assertArraysEqual(map([1, 2, 3], x => x * 2), [2, 4, 6]);
+
+
+module.exports = map;
+
